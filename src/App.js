0f3728c64fccbb8b40dd0c77ddee6d23262d47c8
shirishatyny <shirisha.kolukula@tynybay.io>
2023-03-09 17:40:50 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "./App.css"
+import Trip from './Components/Trip';
+import Data from './Components/Data';
+
+function App(){
+  const dataset = Data.map(myDataFunction)
+  function myDataFunction(elementItems) {
+    return(
+      <Trip
+        details = {elementItems}
+       />
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='react--app--container'>
+      <div className="nav--cont--bar">
+        <img  className="imgClass" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnOnsTBiMPLH_zx9nzLqjylpeJTV4W3fy71VXpFxevvg&s.jpg" />
+      <h3 className="titleClass">Travel journal</h3></div>
+      <div className="app--container">
+        {dataset}
+      </div>
     </div>
   );
 }
